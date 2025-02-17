03aa5fac1bedb6ffbb06a0b116cc2235960b5626
MINGO <skrhkd7@gmail.com>
2025-02-18 01:19:52 +0900

    init2

---
+++ src/main/java/com/application/getinitline/GetInitLineApplication.java
@@ -0,0 +1,13 @@
+package com.application.getinitline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetInitLineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetInitLineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/application/getinitline/GetInitLineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.getinitline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetInitLineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
