814c8831ce6a80e99c78d858b91017ad2b8c54ee
MINGO <skrhkd7@naver.com>
2025-06-07 13:05:34 +0900

    Refactor: Part4 - ch 02 - 05JPA테스트 중요
    - 엔티티의 동등성 비교를 id 기준으로 직접 구현
    - 롬복(EqualsAndHashCode) 사용하지 않고 필요 로직에 맞게 동등성 비교 정의
    - hashCode 구현 시 영속화 안 된 엔티티에 대한 처리 추가
    - 테스트 코드에 Place, Event 엔티티 생성 유틸리티 메서드 추가
    - EventRepository 테스트 코드에서 기본 동작 검증 로직 수정 및 보완

--- src/main/java/com/application/getinitline/domain/Place.java
+++ src/main/java/com/application/getinitline/domain/Place.java
@@ -2,6 +2,7 @@ package com.application.getinitline.domain;
 
 import com.application.getinitline.constant.PlaceType;
 import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -12,7 +13,30 @@ import java.time.LocalDateTime;
 import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
-
+/*
+*  entity가 동일하다는 의미?
+*  일반적인 데이터 오브젝트랑 달리 entity는 "모든 필드가 다 동일하다는걸 검사를 해야 동등하다" 이렇게 평가할 필요가 없다.
+*  persistence context가 관리를 하는 영속성 관리를 해주는 entity는 제대로 등록이 됐다면,
+*  id기준으로 등록이 된다. 즉 id가 같으면 같은 객체인것이다.
+*  그렇기 때문에 entity경우는 dto와 다르게, 기준이 id이기 떄ㅜㅁㄴ에 equals 작성시, id만 구현을 하면된다.
+* 엔티티의 동등성 비교를 편하게 하려고 롬복을 도입했는데
+* 엔티티는 자바의 데이터 클래스랑은 다른 특성을 이용해
+* 이 동등성 기준을 각 필드 전체 비교에서 `id` 비교로 하고자 함
+* 즉 `a(id=1)`, `b(id=1)` 일 때, `a.equals(b) == True`
+* 그러나 id는 persistence context가 관리하므로
+* 서비스 코드에서 처음 만들 땐 `null` 인데,
+* 롬복이나 ide 자동 작성 코드는 이 부분을 `true`로 처리함
+* 즉 `a(id=null)`, `b(id=null)` 일 때, `a.equals(b) == True`
+* 그냥 보면 맞는 것 같지만 실은 둘이 단지 아직 `id`를
+* 부여받지 못했을 뿐, 다른 데이터를 가진 서로 다른 엔티티일 수 있기 때문에, `equals()` 직접 구현이 필요.
+* 한편 `hashCode()`도 구현해줘야 하는데 동등성 기준을 `id`로 잡는다면 `hashCode()` 기준값도
+* `id`를 이용해 다양한 수를 만들면 좋지만 위와 같은 이유로 영속화가 안된 엔티티는 아직 `id`가 없으므로
+* 그냥 상수를 넣든지, 다른 방법이 필요함 상수를 넣어도 큰 무리 없지만 좀 더 효율적으로 만들기 위해
+* 각 엔티티 인덱스 컬럼을 활용해 `hash()` 생성
+*
+* */
+// 하지만 set같은 자료구조에 넣을수없기때문에, entity경우 EqualsAndHashCode 자체를 만들지 않는다.
+//@EqualsAndHashCode(onlyExplicitlyIncluded = true) // true인경우 내가 equals를 직접 구체적으로 명시한것만 넣게다는 뜻이다.
 @Getter
 @ToString
 @Table(indexes = {
@@ -26,6 +50,7 @@ import java.util.Set;
 @Entity
 public class Place {
 
+//    @EqualsAndHashCode.Include // 해당 어노테이션이 붙은 컬럼만 equals를 반영하겠다는 의미이다.
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -109,14 +134,14 @@ public class Place {
         return new Place(placeType, placeName, address, phoneNumber, capacity, memo);
     }
 
-
+    // 직접구현
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
         if (obj == null || getClass() != obj.getClass()) return false;
         return id != null && id.equals(((Place) obj).getId());
     }
-
+    // 직접구현
     @Override
     public int hashCode() {
         return Objects.hash(placeName, address, phoneNumber, createdAt, modifiedAt);
--- src/test/java/com/application/getinitline/repository/EventRepositoryTest.java
+++ src/test/java/com/application/getinitline/repository/EventRepositoryTest.java
@@ -1,12 +1,19 @@
 package com.application.getinitline.repository;
 
+import com.application.getinitline.constant.EventStatus;
+import com.application.getinitline.constant.PlaceType;
 import com.application.getinitline.domain.Event;
+import com.application.getinitline.domain.Place;
 import com.querydsl.core.BooleanBuilder;
+import org.aspectj.util.Reflection;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -39,15 +46,60 @@ class EventRepositoryTest {
     @DisplayName("[repository][GET] EventRepository")
     @Test
     void givenEventRepository_whenGetEvent_thenReturnEvent() {
+
         // Given
-    testEntityManager.persist()
+        Place place = createPlace();
+        Event event = createEvent(place);
+
+        testEntityManager.persist(place);
+        testEntityManager.persist(event);
 
         // When
         Iterable<Event> events =  sut.findAll(new BooleanBuilder());
 
         // Then
-        assertThat(events).contains();
+//        assertThat(events).contains();
+        assertThat(events).hasSize(7);
 
+    }
+
+    private Event createEvent(Place place) {
+        return createEvent(
+            place
+            ,"test event"
+            , EventStatus.ABORTED
+            , LocalDateTime.now()
+            , LocalDateTime.now());
+    }
+
+
+    private Event createEvent(
+//        long id
+//        , long placeId
+        Place place
+        , String eventName
+        , EventStatus eventStatus
+        , LocalDateTime eventStartDatetime
+        , LocalDateTime eventEndDatetime) {
+
+        //ReflectionTestUtils 사용 안감
+//        Event event = Event.of( place
+//            , eventName
+//            , eventStatus
+//            , eventStartDatetime
+//            , eventEndDatetime
+//            ,0
+//            ,24
+//            ,"마스크 착용하세요");
+//        ReflectionTestUtils.setField(event, "id", id);
+        return Event.of(place, eventName, eventStatus, eventStartDatetime, eventEndDatetime,0,24,"마스크 착용하세요");
+    }
+
+
+    private Place createPlace() {
+        Place place = Place.of(PlaceType.COMMON,"test place", "test address", "010-1234-5678", 100, "test memo");
+        ReflectionTestUtils.setField(place, "id", 1L);
+        return place;
     }
 
 
